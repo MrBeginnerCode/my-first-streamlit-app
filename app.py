1320d553f2858d5dfcac4912e13eacd9b2ec2a0b
MrBeginnerCode <79265388+MrBeginnerCode@users.noreply.github.com>
2023-03-17 22:37:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-
+import numpy as np
 import sklearn
 import pickle
 filename = 'model.pickle'
@@ -8,6 +8,7 @@ filename = 'model.pickle'
 model = pickle.load(open(filename, "rb"))
 st.title("Revenue Prediction")
 x = st.number_input('Input Temperature')
+x = np.array(x)
 x = x.reshape(1,-1)
 if st.button('Predict'):
   y_pred = model.predict(x)
